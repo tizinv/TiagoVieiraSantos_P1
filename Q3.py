94763a635235ab1d64c628c7d82b7ed63a1860bc
tizinv <54650883+tizinv@users.noreply.github.com>
2019-10-02 14:09:02 -0300

    Create Q3.py

---
+++ Q3.py
@@ -0,0 +1,10 @@
+# Autor: Tiago Vieira Santos	
+# Valor de Pi
+for cont1 in range(1, 100000001, 2):
+    cont1**(-1)	
+    lista1 = []		
+    list.append(cont1)
+lista2 = [1, -1]* 100000000	
+listaf = lista1 * lista2	
+Valorpi = 4 * sum(listaf)	
+print("O valor de pi = {}",.format(Valorpi))
