761df7b91417c231a7e3c98686a6c79377756b7c
tizinv <54650883+tizinv@users.noreply.github.com>
2019-10-02 14:18:23 -0300

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,12 +1,8 @@
 # Autor: Tiago Vieira Santos
 def Nob(entrada1, entrada2):
-    if entrada2 > entrada1:
-        return Nob(entrada2, entrada1)
-    resultado = entrada1 - entrada2
+    resultado = (entrada1//10) + (entrada1%10) + (entrada2//10) + (entrada2%10)
     return resultado
 entrada1 = int(input("Entre o primeiro valor:\n"))
 entrada2 = int(input("Entre o segundo valor:\n"))
-print("O resultado do enigma de Nob entre {} e {} = {}",.format(entrada1, entrada2, Nob(entrada1, entrada2)))
-
-
-Nob()
+print("O resultado do enigma de Nob entre {} e {} = {}".format(entrada1, entrada2, Nob(entrada1, entrada2)))
+print("Ex:O resultado do enigma de Nob entre {} e {} = {}".format(entrada1, entrada2, Nob(21, 36)))
