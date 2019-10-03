4cda59a43d6f23030a355e7c66ce4afb94f9d557
tizinv <54650883+tizinv@users.noreply.github.com>
2019-10-03 20:49:03 -0300

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -2,7 +2,12 @@
 def Nob(entrada1, entrada2):
     resultado = (entrada1//10) + (entrada1%10) + (entrada2//10) + (entrada2%10)
     return resultado
-entrada1 = int(input("Entre o primeiro valor:\n"))
-entrada2 = int(input("Entre o segundo valor:\n"))
-print("O resultado do enigma de Nob entre {} e {} = {}".format(entrada1, entrada2, Nob(entrada1, entrada2)))
-print("Ex:O resultado do enigma de Nob entre {} e {} = {}".format(entrada1, entrada2, Nob(21, 36)))
+
+def main():
+    entrada1 = int(input("Entre o primeiro valor:\n"))
+    entrada2 = int(input("Entre o segundo valor:\n"))
+    print("O resultado do enigma de Nob entre {} e {} = {}".format(entrada1, entrada2, Nob(entrada1, entrada2)))
+    print("Ex:O resultado do enigma de Nob entre {} e {} = {}".format(21, 36, Nob(21, 36)))
+    
+    
+main()
