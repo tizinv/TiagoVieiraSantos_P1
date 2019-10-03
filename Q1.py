733c7320974cfac7a786551c89121327e90ea5fa
tizinv <54650883+tizinv@users.noreply.github.com>
2019-10-03 20:34:31 -0300

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -4,12 +4,17 @@ import math
 def segundograu(a,b,c):
   delta = (b**2) - (4 * a * c)
   if delta < 0:
-    return (1, "raiz complexa", "raiz complexa")
+    print ("Raízes Complexas = " + str(-b / (2 * a)) + "+-" + str(math.sqrt(-delta) / (2 * a)) + "i")
+    return 1
   raiz1 = (-b + math.sqrt(delta)) / 2*a 
   raiz2 = (-b - math.sqrt(delta)) / 2*a
   return (0, raiz1, raiz2)
-a = int(input("Entre com o parâmetro a:\n"))
-b = int(input("Entre com o parâmetro b:\n"))
-c = int(input("Entre com o parâmetro c:\n"))
-print("Modelo(1 = raízes complexas/ 0 = raízes reais, raiz1, raiz2)")
-print(segundograu(a,b,c))
+def main():
+  a = int(input("Entre com o parâmetro a:\n"))
+  b = int(input("Entre com o parâmetro b:\n"))
+  c = int(input("Entre com o parâmetro c:\n"))
+  print("Se o Resultado for 1 = raízes complexas\nSe o Resultado for 0 = raízes reais \n\n\t\t\t\tRaiz1, Raiz2")
+  print("Resultado = ", segundograu(a,b,c))
+
+  
+main()
